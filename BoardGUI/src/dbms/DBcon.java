b6849b73c3c52f662508468b3840332ddfef8d20
chani <chani@Choiy.mshome.net>
2026-01-05 17:50:41 +0900

    2026.01.05

---
+++ BoardGUI/src/dbms/DBcon.java
@@ -0,0 +1,24 @@
+package dbms;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DBcon {
+
+	public static void main(String[] args) {
+		String driver = "org.mariadb.jdbc.Driver";
+		String serverUrl = "jdbc:mariadb://localhost:3307/";
+		String dbName = "BoardDB";
+		String username = "root";
+		String password = "1234";
+		
+		Connection con = null;
+		Statement stmt = null;
+		
+		try {
+			
+		} catch () {
+			
+		}
+	}
+}
