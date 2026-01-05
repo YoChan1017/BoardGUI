d1185aff1eefdc0f975ac7b055e18e16e4e4b934
chani <chani@Choiy.mshome.net>
2026-01-05 18:07:41 +0900

    2026.01.05_01

--- BoardGUI/src/dbms/DBcon.java
+++ BoardGUI/src/dbms/DBcon.java
@@ -1,6 +1,7 @@
 package dbms;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class DBcon {
@@ -16,6 +17,28 @@ public class DBcon {
 		Statement stmt = null;
 		
 		try {
+			// Driver Load
+			Class.forName(driver);
+			System.out.println("Driver Load Success!");
+			
+			// Server Connection
+			con = DriverManager.getConnection(serverUrl, username, password);
+			System.out.println("Server Connection Complete.");
+			
+			// Database 생성/확인
+			stmt = con.createStatement();
+			String sql = "CREATE DATABASE IF NOT EXISTS" + dbName;	// DB가 존재하지 않을 시 생성, 존재하는 경우 경고 메세지
+			stmt.executeUpdate(sql);
+			System.out.println("Database '" + dbName + "' created or already exists.");
+			
+			// 기존 연결 해제
+			stmt.close();
+			con.close();
+			
+			// 생성된 BoardDB로 재연결
+			String targetUrl = serverUrl + dbName;
+			con = DriverManager.getConnection(targetUrl, username, password);
+			System.out.println("Successfully connected to '" + dbName + "'");
 			
 		} catch () {
 			
