55ac6f09fb7ff91ec330dce5cee483255d67d20f
chani <chani@Choiy>
2026-01-15 20:43:14 +0900

    2026.01.15_03

---
+++ BoardGUI/src/gui/board/boards/BoardGUI.java
@@ -0,0 +1,27 @@
+package gui.board.boards;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class BoardGUI extends JFrame implements ActionListener {
+
+	// 게시판 GUI
+	// 상단에 게시판 이름 표시
+	// 중단에 게시글 목록 표시
+	// 하단에 MainGUI처럼 버튼 생성
+	
+	public BoardGUI() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
